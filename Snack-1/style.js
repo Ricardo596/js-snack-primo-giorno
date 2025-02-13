1373faa21a656f5dbf89f71d0d42dbbe1ab8e981
Ricardo596 <ricardodiotti8@gmail.com>
2025-02-13 10:43:17 +0100

    Non ne sono convinto

---
+++ Snack-1/style.js
@@ -0,0 +1,12 @@
+/*L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore*/
+
+const number = parseFloat (prompt("10"));
+const numbertwo = parseFloat(prompt("5"));
+
+if (number > numbertwo){
+ console.log ("il numero maggiore è" + number)
+
+};
+else if (number < numbertwo) {
+    console.log("il numero maggiore è" + numbertwo)
+}
