344502c5e034d249c09aa6abc948df40538b57c0
Ricardo596 <ricardodiotti8@gmail.com>
2025-02-13 12:53:27 +0100

    C'è sempre un errore D:

---
+++ Snack-1/Snack-2/snack-4/file.js
@@ -0,0 +1,12 @@
+/* Crea un array vuoto. Chiedi per 6 volte all utente di inserire un numero, se è dispari inseriscilo
+nell array. */
+
+let matematica = []
+
+for (let i = 0; i< 6; i++){
+    cost userNumb = matematica (prompt("type a number"))
+};
+if (numero % 2 !== 0) {
+    numeriDispari.push(numero);
+}
+    console.log(matematica)
